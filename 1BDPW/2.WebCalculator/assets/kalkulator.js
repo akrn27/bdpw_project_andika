6ed1c540404d41b628236e05f55b2894e884f40b
Akrn27 <akurnia755@gmail.com>
2022-06-23 00:17:18 +0700

    finish web storage

--- 1BDPW/2.WebCalculator/assets/kalkulator.js
+++ 1BDPW/2.WebCalculator/assets/kalkulator.js
@@ -60,7 +60,15 @@ if(calculator.operator === '+') {
 }else {
     result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
 }
+const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result
+}
+putHistory(history);
 calculator.displayNumber = result;
+renderHistory();
 }
 
 const buttons = document.querySelectorAll('.button');
